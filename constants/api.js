6103c50eeb0a9e81bc3b32cc932ea141fa41dc86
TheRak16hz <sebas.elio2004@gmail.com>
2025-06-10 22:22:23 -0400

    first commit

---
+++ constants/api.js
@@ -0,0 +1,9 @@
+export const API_URL = "http://172.22.224.1:3000/api";
+// Replace with your actual API URL
+
+
+//remember to change the API_URL and .ENV file
+
+//hosted on Render https://react-native-bookworm-p67w.onrender.com/api
+//localhost is your pc ip, so in this case it is 172.22.224.1 the port is 3000
+//LOCAL http://localhost:3000/api remember run it with npm run dev
